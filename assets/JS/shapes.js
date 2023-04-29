bdbe55dedf2fe163e487f993c52009d761a62bf7
7K <kmanfinkel17@gmail.com>
2023-04-29 13:51:10 -0500

    "SVG test created SVG elements, set up index var

--- assets/JS/shapes.js
+++ assets/JS/shapes.js
@@ -2,7 +2,38 @@
 class Shape {
     constructor(color){
         this.color = color;
-        const height = 200;
-        const width = 200;
     }
-}
+   
+};
+
+// Circle
+class Circle extends Shape{
+    createShape(){
+        return `< circle cx="50%" cy="50%" r="100" height="100%" width="100%" fill="${this.color}/>`;
+    }
+};
+
+// Square
+class Square extends Shape{
+    createShape(){
+        return `< rect x="50" height="200" width="200" fill="${this.color}/>`;
+    }
+};
+
+// Triangle
+class Triangle extends Shape{
+    createShape(){
+        return `< polygon height="100%" width="100%" points="0,200 200,200 100,0" fill="${this.color}/>`;
+    }
+};
+
+// Shield
+class Shield extends Shape{
+    createShape(){
+        return `<polygon height="100%" width="100%" points="0,0 200,0 200,100 100,200 0,100"  fill="${this.color}"/>`;
+    }
+};
+
+
+// export
+module.exports = {Circle, Square, Triangle, Shield};
--- index.js
+++ index.js
@@ -1,48 +1,73 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
-const jest = require('jest');
 const shapes = require('./assets/JS/shapes.js')
 
-// constructor for test
-function Index() {};
+// create SVG class
+class SVGMaker {
+    constructor(){
+        this.text = text;
+        this.shape = shape;
+    }
+    createSVG(){
+        return `<svg version="1.1" xmlns="http://www.w3.org/2000/svg" viewbox="0 0 200 200" width="200" height="200">${this.textEl} ${this.shapeEl}</svg>`
+    }
+    setText(text, textColor){
+        this.textEl = `<text font-weight="bold" x="100" y="100" font-size="70" text-anchor="middle" fill="${textColor}">${text}</text>`
+
+    }
+    setShape(shape, shapeColor){
+        this.shapeEl = shape.createShape();
+    }
+}
 // Terminal prompt section
 const getLogoInfo = () =>{inquirer.prompt([
     {
         type: 'input',
         name: 'text',
-        message: 'Please enter your logo acronym in 3 characters or less'
+        message: 'Please enter your logo acronym in 3 characters or less:',
+        validate: confirmTextLength
     },
     {
-        type: 'list',
+        type: 'input',
         name: 'textColor',
-        message: 'Please enter your desired text color',
+        message: 'Please enter your desired text color via color keyword or in hexadecimal:',
         choices: ['','','',''],
         default: choices[0],
-        when: (responses) => responses.text.length > 0 && responses.text.length <= 3
+        validate: colorVerify
     },
     {
         type: 'list',
         name: 'shape',
         message: 'Please enter your desired logo shape',
-        choices: ['circle','square','triangle','shield'],
-        default: choices[0],
-        when: (responses) => responses.textColor 
+        choices: ['Circle','Square','Triangle','Shield'],
+        default: choices[0]
     },
     {
-        type: 'list',
+        type: 'input',
         name: 'shapeColor',
         message: 'Please enter your desired shape color',
         choices: ['','','',''],
-        default: choices[0]
+        validate: colorVerify
     },
 
 ]).then((responses) =>{
     return;
 })
 };
+
+// Q1 validator
+function confirmTextLength(){
+    if((input.trim() != "") && (input.trim().length <= 3)){
+        return true;
+    }
+    return "Please enter an input of 1 to 3 characters"
+};
+
+// Q2 validator
+function colorVerify(){
+
+};
 // text(limit char), text color(list), shapes(list),shape color(list), output'logo generated', creates ''.svg file, 300x200 pixel image
 getLogoInfo();
 // backend creation section
 
-// export
-module.exports = Index;
